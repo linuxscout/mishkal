5866e554530aeae91d2b1e7f4db44d43fe00f9d5
zerrouki <taha.zerrouki@gmail.com>
2015-01-25 16:28:19 +0100

    add a script to test aljazeera vocalized texts
    add a script to test aljazeera vocalized texts

---
+++ tests/test_tashkeelCompare_jazeera.sh
@@ -0,0 +1,13 @@
+#~FILE="adab.txt"
+#FILE="rndlines.txt"
+FILE="aljazeera.txt"
+python -m cProfile ../mishkal-console.py -p -c -l 500 -f samples/vocalized/${FILE} >output/compare/rndlines.11.txt
+
+DATE=`date +%Y-%m-%d-%H:%M`
+echo "make archive" 
+cp output/compare/rndlines.11.txt  output/compare/L${DATE}.txt
+echo "save stats"
+date >> output/compare/file.stats  
+grep "function calls" -2 -h output/compare/rndlines.11.txt | sed 's/^.*function .* in //g;s/:\*.*$//g' | sed 'N;s/\n//g' >> output/compare/file.stats
+tail -n 3 output/compare/file.stats
+
